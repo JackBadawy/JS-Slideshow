0359a61f762c151f04ca763a480c26f435a0035c
Jack <jackbadawy1999@gmail.com>
2023-09-13 14:53:02 +1000

    applied animation to all image changes without the need for multiple img tags

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let pictureNumber = 1;
-const slideshowImageElement = document.getElementById(
+/* const slideshowImageElement = document.getElementById(
   "slideshow_image_element"
-);
+); */
+const mainElement = document.getElementsByTagName("main");
+
 const slideshowPreviousPictureButton = document.getElementById(
   "previous_picture_button"
 );
@@ -9,8 +11,19 @@ const slideshowNextPictureButton = document.getElementById(
   "next_picture_button"
 );
 
+const slideshowImageElement = document.createElement("img");
+slideshowImageElement.id = "slideshow_image_element";
+slideshowImageElement.classList.add("fade-in");
+slideshowImageElement.src = `img/img${pictureNumber}.png`;
+mainElement[0].insertBefore(slideshowImageElement, slideshowNextPictureButton);
+
 function slideshowCurrentPicture() {
   slideshowImageElement.src = `img/img${pictureNumber}.png`;
+  slideshowImageElement.remove();
+  mainElement[0].insertBefore(
+    slideshowImageElement,
+    slideshowNextPictureButton
+  );
 }
 
 slideshowPreviousPictureButton.addEventListener("click", () => {
